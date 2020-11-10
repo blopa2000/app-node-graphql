1406a0ea0c9f2931a9b6f62bd59b945ffd44df39
blopa2000 <jpagudelo2000@gmail.com>
2020-11-10 10:51:08 -0500

    Docker file settings

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-# rest-node-docker
+# rest-node
 
 ## Docker install
+
 ```
 docker-compose build
 docker-compose up
 ```
+
 ## npm install
+
 ```
 npm i
 npm run dev
--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGO_URL,{
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
