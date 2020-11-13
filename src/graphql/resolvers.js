4a28377856a718c3dcda078d9e2dac8bf5fdbfc3
blopa2000 <jpagudelo2000@gmail.com>
2020-11-13 16:37:11 -0500

    modify and delete courses

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,23 +1,77 @@
-import Courses from "../models/courses";
-
 export default {
   Query: {
     async getCourses(_, args, { models }) {
       return await models.Courses.find();
     },
+
     async getCourse(root, { _id }, { models }) {
       return await models.Courses.findOne({ _id });
+    },
+
+    async getTeachers(_, args, { models }) {
+      return await models.Teachers.find();
+    },
+
+    async getTeacher(_, { id }, { models }) {
+      return await models.Teachers.findById(id);
     },
   },
 
   Mutation: {
-    async createCourse(_, { input }) {
-      const newCourse = new Courses(input);
-      return await newCourse.save();
+    async createCourse(_, { input }, { models }) {
+      const newCourse = new models.Courses(input);
+      const saveCourse = await newCourse.save();
+
+      await models.Teachers.updateOne(
+        { _id: input.teacher },
+        { $push: { courses: saveCourse._id } }
+      );
+
+      return saveCourse;
     },
+
     async createTeacher(_, { input }, { models }) {
       const newTeacher = new models.Teachers(input);
       return await newTeacher.save();
+    },
+
+    async updateTeacher(_, { id, input }, { models }) {
+      return await models.Teachers.findOneAndUpdate(
+        { _id: id },
+        { $set: { ...input } },
+        { new: true }
+      );
+    },
+
+    async updateCourse(_, { id, input }, { models }) {
+      return await models.Courses.findOneAndUpdate(
+        { _id: id },
+        { $set: { ...input } },
+        { new: true }
+      );
+    },
+
+    async DeleteCourse(_, { idCourse, idTeacher }, { models }) {
+      await models.Teachers.updateOne({ _id: idTeacher }, { $pull: { courses: idCourse } });
+      await models.Courses.deleteOne({ _id: idCourse });
+      return await models.Courses.find();
+    },
+  },
+
+  Courses: {
+    async teacher({ teacher }, args, { models }) {
+      return await models.Teachers.findById(teacher);
+    },
+  },
+
+  Teachers: {
+    async courses({ courses }, args, { models }) {
+      const allCourses = [];
+      for (const course of courses) {
+        const data = await models.Courses.findOne({ _id: course });
+        allCourses.push(data);
+      }
+      return allCourses;
     },
   },
 };
